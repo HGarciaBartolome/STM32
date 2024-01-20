9ffe1c915c74f589334e20316b0ee970062baf19
HGarciaBartolome <h.garciab@alumnos.upm.es>
2024-01-20 21:23:32 +0100

    pullup

--- Project/Core/Src/main.c
+++ Project/Core/Src/main.c
@@ -117,7 +117,7 @@ void Pin_Input(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin){
 	GPIO_InitTypeDef GPIO_InitStruct={0};
 	GPIO_InitStruct.Pin = GPIO_Pin;
 	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-	GPIO_InitStruct.Pull = GPIO_PULLUP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
 }
 
